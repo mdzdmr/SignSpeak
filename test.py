771cc887bbf50d7a99d20032a3b22cae2f996ca6
Mohammed Zaid Mir <mzaidmir111@gmail.com>
2024-07-03 00:27:41 -0400

    create-test.py

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import math
-import time
 
 import cv2
 import numpy as np
 
 from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
 
 cap = cv2.VideoCapture(0)  # 0 is the ID no. of the webcam
 detector = HandDetector(maxHands=1)
+classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
 
 OFFSET = 20
 IMG_SIZE = 300
@@ -15,15 +16,18 @@ IMG_SIZE = 300
 folder = "Data/C"
 counter = 0
 
+labels = ["A", "B", "C"]
+
 while True:
     success, img = cap.read()
+    imgOutput = img.copy()
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
         x, y, w, h = hand["bbox"]
 
         imgWhite = np.ones((IMG_SIZE, IMG_SIZE, 3), np.uint8) * 255
-        imgCrop = img[y - OFFSET:y + h + OFFSET, x - OFFSET: x + w + OFFSET]
+        imgCrop = img[y-OFFSET:y+h+OFFSET, x-OFFSET: x+w+OFFSET]
 
         imgCropShape = imgCrop.shape
 
@@ -36,6 +40,8 @@ while True:
             imgResizeShape = imgResize.shape
             wGap = math.ceil((IMG_SIZE - wCal) / 2)
             imgWhite[:, wGap:wCal + wGap] = imgResize
+            prediction, index = classifier.getPrediction(img)
+            print(prediction, index)
         else:
             k = IMG_SIZE / w
             hCal = math.ceil(k * h)
@@ -43,14 +49,14 @@ while True:
             imgResizeShape = imgResize.shape
             hGap = math.ceil((IMG_SIZE - hCal) / 2)
             imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+        cv2.rectangle(imgOutput, (x - OFFSET, y - OFFSET - 50), (x - OFFSET + 90, y - OFFSET - 50 + 50), (255, 0, 255), cv2.FILLED)
+        cv2.putText(imgOutput, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+        cv2.rectangle(imgOutput, (x - OFFSET, y - OFFSET), (x + w + OFFSET, y + h + OFFSET), (255, 0, 255), 4)
 
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
 
     cv2.imshow("Image", img)
-    key = cv2.waitKey(1)
-
-    if key == ord("s"):
-        counter += 1  # To keep track of images we save.
-        cv2.imwrite(f"{folder}/Image_{time.time()}.jpg", imgWhite)
-        print(counter)
+    cv2.waitKey(1)
